ec4d62f9cbb849f60ce1d499898e6c2d7fd342b4
Magnus Egelberg <magnus@egelberg.se>
2013-10-30 15:49:35 +0100

    -

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,15 @@
 class Category < ActiveRecord::Base
   attr_accessible :description, :name, :image, :available
   
-  has_many :rentals, :dependent => :destroy
-  #validates_associated :rentals
+  has_many :rentals
   
   belongs_to :client  
+  after_destroy :cleanup
+  
+  
+  def cleanup
+  	 Rental.where(:category_id => self.id).update_all(:category_id => nil)
+  end
+  
+   
 end
